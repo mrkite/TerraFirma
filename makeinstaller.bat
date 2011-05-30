d3bdb9157ec343f5dfb5125b9fe9e5cc39944ae0
sean@seancode.com <sean@seancode.com>
2011-05-30 03:56:19 -0700

    Made the installer for 1.0

---
+++ makeinstaller.bat
@@ -0,0 +1,3 @@
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" terrafirma.wxs
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension terrafirma.wixobj
+@pause
