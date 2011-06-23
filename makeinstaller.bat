c6e2e59e5151d826eae854f0a07a3ed17a332938
sean@seancode.com <sean@seancode.com>
2011-06-23 01:15:44 -0700

    Changed installer, choose your install dir, add shortcut to desktop.

--- makeinstaller.bat
+++ makeinstaller.bat
@@ -1,3 +1,5 @@
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" MyInstallDirDlg.wxs
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" MyWixUI_InstallDir.wxs
 "c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" terrafirma.wxs
-"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension terrafirma.wixobj
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension -o terrafirma.msi terrafirma.wixobj MyInstallDirDlg.wixobj MyWixUI_InstallDir.wixobj
 @pause
