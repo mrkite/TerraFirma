cea333f54d20617018c5394acb2f7c9c8d9d44bb
Sean <skasun@gmail.com>
2016-05-25 20:15:10 -0700

    prepped for 3.0.7

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   app.installTranslator(&translator);
 
   app.setApplicationName("Terrafirma");
-  app.setApplicationVersion("3.0.6");
+  app.setApplicationVersion("3.0.7");
   app.setOrganizationName("seancode");
 
   MainWindow w;
--- makeinstaller.bat
+++ makeinstaller.bat
@@ -1,4 +1,4 @@
-"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" MyWixUI_InstallDir.wxs
-"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" terrafirma.wxs
-"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension -o terrafirma.msi terrafirma.wixobj MyWixUI_InstallDir.wixobj
+"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" MyWixUI_InstallDir.wxs
+"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" terrafirma.wxs
+"c:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -ext WixUIExtension -o terrafirma.msi terrafirma.wixobj MyWixUI_InstallDir.wixobj
 @pause
