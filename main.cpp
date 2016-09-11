e067f580d933701d866c0db7bcdeb7d8eb6a8ad6
Sean <skasun@gmail.com>
2016-09-11 16:15:46 -0700

    argh

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   app.installTranslator(&translator);
 
   app.setApplicationName("Terrafirma");
-  app.setApplicationVersion("3.0.13");
+  app.setApplicationVersion("3.0.14");
   app.setOrganizationName("seancode");
 
   MainWindow w;
