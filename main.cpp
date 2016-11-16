3c5b4332e4266545fbcc3fb809d2beb3379884ee
enigmaquip <enigmaquip@gmail.com>
2016-11-15 17:48:34 -0700

    Bump version number
    bump to 3.0.16 and fix my merge flub

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   app.installTranslator(&translator);
 
   app.setApplicationName("Terrafirma");
-  app.setApplicationVersion("3.0.11");
+  app.setApplicationVersion("3.0.16");
   app.setOrganizationName("seancode");
 
   MainWindow w;
