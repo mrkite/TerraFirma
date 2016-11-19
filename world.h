92eb59a602fb005c845934b02525f1499c0033f4
enigmaquip <enigmaquip@gmail.com>
2016-11-19 14:28:28 -0700

    1.3.4.3

--- world.h
+++ world.h
@@ -35,7 +35,7 @@ class Tile {
 
 class World : public QObject, public QRunnable {
   static const int MinimumVersion = 88;
-  static const int HighestVersion = 186;
+  static const int HighestVersion = 187;
 
   Q_OBJECT
 
