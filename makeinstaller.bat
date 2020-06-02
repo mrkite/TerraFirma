9efdb163fa1fe7b41e93c7be0e7f9e39fe528bee
Sean <skasun@gmail.com>
2020-06-02 08:50:26 -0700

    updated wix version

--- makeinstaller.bat
+++ makeinstaller.bat
@@ -1,4 +1,4 @@
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" MyWixUI_InstallDir.wxs
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" terrafirma.wxs
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -ext WixUIExtension -o terrafirma.msi terrafirma.wixobj MyWixUI_InstallDir.wixobj
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" MyWixUI_InstallDir.wxs
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" terrafirma.wxs
+"c:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUIExtension -o terrafirma.msi terrafirma.wixobj MyWixUI_InstallDir.wixobj
 @pause
