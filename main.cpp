38cf36ac5856c1e6c3fc68d15aa59f54b4b28394
Sean <skasun@gmail.com>
2020-11-14 19:01:51 -0700

    updated for 1.4.1

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   QApplication::installTranslator(&translator);
 
   QApplication::setApplicationName("Terrafirma");
-  QApplication::setApplicationVersion("3.1.1");
+  QApplication::setApplicationVersion("3.1.2");
   QApplication::setOrganizationName("seancode");
 
   MainWindow w;
