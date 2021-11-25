2abd6496331cb611438bba3139e353f3bd537925
Sean <skasun@gmail.com>
2021-11-25 10:37:51 -0700

    updated for 1.4.3.2

--- world.h
+++ world.h
@@ -35,7 +35,7 @@ class Tile {
 
 class World : public QObject, public QRunnable {
   static const int MinimumVersion = 88;
-  static const int HighestVersion = 242;
+  static const int HighestVersion = 244;
 
 
   Q_OBJECT
